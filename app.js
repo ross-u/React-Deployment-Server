7cfe9478156d31744a7d3d9169543ccd90e3de8c
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2020-03-09 11:30:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose     = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const projectRouter = require('./routes/project-routes');
-const taskRouter = require('./routes/task-routes');
+const projectRouter = require("./routes/project-routes");
+const taskRouter = require("./routes/task-routes");
+require("dotenv").config();
 
 const app = express();
 
 // MONGOOSE CONNECTION
 mongoose
-  .connect('mongodb://localhost/project-management-server', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-app.disable('etag');
+app.disable("etag");
 
 // MIDDLEWARE SETUP
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // CORS SETTINGS TO ALLOW CROSS-ORIGIN INTERACTION:
-app.use(cors({
-  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"] // <== this will be the URL of our React app (it will be running on port 3000)
+  })
+);
 
 // ROUTES MIDDLEWARE:
-app.use('/api', projectRouter);
-app.use('/api', taskRouter);
+app.use("/api", projectRouter);
+app.use("/api", taskRouter);
 
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 module.exports = app;
